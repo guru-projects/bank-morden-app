d43c1eb9b360c0d9896295326acafd352866e80d
Guru <guru85111@gmail.com>
2024-12-08 21:52:55 +0530

    `feat: updated Stats component with new styles, layout and features`

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
+import Styles from '../style'
+import { stats } from '../constants'
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <div className='flex flex-wrap justify-between items-center m-10'>
+    {stats.map((item, index) => (
+      <div className='lex-1 flex justify-start items-center flex-row m-3`' key={index}>
+        <h4 className="font-poppins font-semibold xs:text-[40.89px] text-[30.89px] xs:leading-[53.16px] leading-[43.16px] text-white">{ item.value }</h4>
+        <p className="font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] text-gradient uppercase ml-3">{ item.title}</p>
+      </div>
+    ))}
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,6 @@ module.exports = {
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba(9, 151, 124, 0.1)",
       },
-      backgroundImage: {
-                'my-gradient': 'radial-gradient(64.18% 64.18% at 71.16% 35.69%, #def9fa 0.89%, #bef3f5 17.23%, #9dedf0 42.04%,#7de7eb 55.12%,#5ce1e6 71.54%,#33bbcf 100%)',
-            },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
