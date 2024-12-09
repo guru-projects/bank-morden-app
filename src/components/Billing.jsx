bc28be019382f72a39490859f13bc0605f2f84bc
Guru <guru85111@gmail.com>
2024-12-09 11:37:50 +0530

    `Refactor Billing component: add layout, images, and text content`

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { bill, apple, google } from "../assets";
+import Styles, { layout } from "../style";
+// import layout from '../style'
 
-const Billinj = () => {
-  return (
-    <div>Billinj</div>
-  )
-}
+const Billing = () => (
+  <section id="billing" className={`${layout.sectionReverse} md:items-center`}>
+    <div className="flex-1">
+      <h2 className={`${Styles.heading2} text-white`}>
+        Easily control your <br /> billing & invoicing.
+      </h2>
+      <p
+        className={`${Styles.paragraph} max-w-[570px] mt-6 mr-[52px] mb-[48px]`}>
+        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean
+        neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+      </p>
+      <div className="flex gap-4">
+        <img src={apple} alt={apple} />
+        <img src={google} alt={google} />
+      </div>
+    </div>
+    <div className={`${layout.sectionImgReverse} flex-1 space-y-4`}>
+      <img src={bill} alt={bill} className="max-h-[500px]" />
+    </div>
+  </section>
+);
 
-export default Billinj
+export default Billing;
--- src/style.js
+++ src/style.js
@@ -13,13 +13,11 @@ const styles = {
   
     marginX: "sm:mx-16 mx-6",
     marginY: "sm:my-16 my-6",
-
-    button: 'w-[170px] h-[64px] bg-blue-gradient rounded-[10px] font-medium font-poppins text-[18px]'
   };
   
   export const layout = {
     section: `flex md:flex-row flex-col ${styles.paddingY}`,
-    sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
+    sectionReverse: `flex md:flex-row-reverse flex-col-reverse ${styles.paddingY}`,
   
     sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
     sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
