dc6cff666e56188a9d8cc1683361edf272915c3a
Guru <guru85111@gmail.com>
2024-12-10 09:34:55 +0530

    updated  clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+import {clients} from '../constants'
+import Styles from '../style'
+const Clients = () => (
+  <section className={`${Styles.paddingY} flex md:justify-between justify-center flex-wrap gap-6`}>
+    {clients.map((item, index) => (
+      <img key={index} src={item.logo} alt={item.logo} className='w-[190px] h-[60px] object-contain' />
+    ))}
+  </section>
+)
 
 export default Clients
