3136f8d863a4d64457d489aeab665703e2668b1c
Guru <guru85111@gmail.com>
2024-12-10 18:09:52 +0530

    `Added gradient overlays to billing section image`

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -18,8 +18,13 @@ const Billing = () => (
         <img src={google} alt={google} />
       </div>
     </div>
-    <div className={`${layout.sectionImgReverse} flex-1 space-y-4`}>
-      <img src={bill} alt={bill} className="max-h-[500px]" />
+    <div className={`${layout.sectionImgReverse} flex-1 space-y-4 relative`}>
+      <img src={bill} alt={bill} className="max-h-[500px] z-[5]" />
+
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
     </div>
   </section>
 );
